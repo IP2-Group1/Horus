4a3e76f4791c2abca5b0dc151212394e8d2a035e
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-07-15 15:55:56 +0300

    update time live

--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,4 @@
-//add code here
+s//add code here
 const time = document.getElementById('time');
 const date = document.getElementById('date');
 const currentWeatherItems = document.getElementById('current-weather-items');
@@ -6,3 +6,16 @@ const timezone = document.getElementById('time-zone');
 const country = document.getElementById('country');
 const weatherForecast = document.getElementById('weather-forecast');
 const currentTemp = document.getElementById('current-temp');
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >=13 ? hour % 12 : hour;
+    const minutes = time.getMinutes();
+    const aMpM = hour >=12 ? 'PM' : 'AM';
+
+    time.innerHTML = hoursIn12HrFormat + ':'+minutes+ ' ' + '<span id = "am-pm>"$(ampm)</span>'
+},1000)
