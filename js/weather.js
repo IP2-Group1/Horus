7a66ec5cb58b78c5f985f9bd06919229af50ece7
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-07-15 16:14:45 +0300

    update date live

--- js/weather.js
+++ js/weather.js
@@ -7,10 +7,14 @@ const country = document.getElementById('country');
 const weatherForecast = document.getElementById('weather-forecast');
 const currentTemp = document.getElementById('current-temp');
 
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 setInterval(() => {
     const timeObject = new Date();
     const month = timeObject.getMonth();
-    const date = timeObject.getDate();
+    const date1 = timeObject.getDate();
     const day = timeObject.getDay();
     const hour = timeObject.getHours();
     const hoursIn12HrFormat = hour >=13 ? hour % 12 : hour
@@ -18,4 +22,6 @@ setInterval(() => {
     const ampm = hour >=12 ? 'PM' : 'AM'
 
     time.innerHTML = hoursIn12HrFormat + ':'+ minutes+ ' ' + `<span id="am-pm">${ampm}</span>`
+
+    date.innerHTML = days[day] + ', ' + date1+ ' ' + months[month]
 },1000)
