e71437e7e0d821088a819df8c96e021be415284d
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-07-15 15:49:17 +0300

    get html elements

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,8 @@
 //add code here
+const time = document.getElementById('time');
+const date = document.getElementById('date');
+const currentWeatherItems = document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const country = document.getElementById('country');
+const weatherForecast = document.getElementById('weather-forecast');
+const currentTemp = document.getElementById('current-temp');
