201e73c9c47b61bdf86e1d865eed21c0795c0dcf
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-07-15 16:09:38 +0300

    update time live

--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,4 @@
-s//add code here
+//add code here
 const time = document.getElementById('time');
 const date = document.getElementById('date');
 const currentWeatherItems = document.getElementById('current-weather-items');
@@ -8,14 +8,14 @@ const weatherForecast = document.getElementById('weather-forecast');
 const currentTemp = document.getElementById('current-temp');
 
 setInterval(() => {
-    const time = new Date();
-    const month = time.getMonth();
-    const date = time.getDate();
-    const day = time.getDay();
-    const hour = time.getHours();
-    const hoursIn12HrFormat = hour >=13 ? hour % 12 : hour;
-    const minutes = time.getMinutes();
-    const aMpM = hour >=12 ? 'PM' : 'AM';
+    const timeObject = new Date();
+    const month = timeObject.getMonth();
+    const date = timeObject.getDate();
+    const day = timeObject.getDay();
+    const hour = timeObject.getHours();
+    const hoursIn12HrFormat = hour >=13 ? hour % 12 : hour
+    const minutes = timeObject.getMinutes();
+    const ampm = hour >=12 ? 'PM' : 'AM'
 
-    time.innerHTML = hoursIn12HrFormat + ':'+minutes+ ' ' + '<span id = "am-pm>"$(ampm)</span>'
+    time.innerHTML = hoursIn12HrFormat + ':'+ minutes+ ' ' + `<span id="am-pm">${ampm}</span>`
 },1000)
